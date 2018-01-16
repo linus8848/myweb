ae82d36c17f61fa000cea476756f3b7614a068eb
395059821@qq.com <395059821@qq.com>
2018-01-16 09:15:54 +0000

    zhongwen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'myweb') - laibin's local web</title>
+    <title>@yield('title', 'myweb') - 使用Laravel制作论坛</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
